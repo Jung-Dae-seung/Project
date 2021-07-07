8f400dcc8bbe99ffd08d878f74526fac7b87ddf8
shjgy1 <shjgy1@nate.com>
2021-07-07 15:13:56 +0900

    home 컨트롤러 login 추가 (송해준 07.07)

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -1,13 +1,11 @@
 package com.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,6 +24,13 @@ public class HomeController {
 		
 
 		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String loginGet() {
+		logger.info("login page");
+		
+		return "login";
 	}
 	
 }
