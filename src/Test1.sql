6750a305ad285b2952859c0f6dd99744dbe6b7bd
shjgy1 <shjgy1@nate.com>
2021-07-23 03:00:34 +0900

    sql 한글 깨짐 테스트

--- src/Test1.sql
+++ src/Test1.sql
@@ -1,5 +1,4 @@
-
---��� ���̺�
+--멤버 테이블
 create table memberTBL(userid varchar2(15) constraint memberTBL_pk primary key,
 						password varchar2(100) not null,
 						name varchar2(20) not null,
@@ -8,7 +7,7 @@ create table memberTBL(userid varchar2(15) constraint memberTBL_pk primary key,
                         mobile varchar2(15) not null,
 						email varchar2(50));
 
--- ���� ���̺�    
+-- 리뷰 테이블    
 create table reviewTBL(reviewer varchar2(15) not null,
                         bno number(10,0) constraint reviewTBL_pk primary key,
                         review varchar2(2000),
@@ -17,11 +16,11 @@ create table reviewTBL(reviewer varchar2(15) not null,
                         updatedate date default sysdate,
                         restaurent varchar2(100) not null);
                         
--- �������̺� ������
+-- 리뷰테이블 시퀀스
 create sequence seq_review;
 
                 
--- �����Խ��� ���̺�
+-- 자유게시판 테이블
 create table freeBoardTBL(bno number(10,0) constraint freeBoardTBL_pk primary key,
                       title varchar2(200) not null,
                       content varchar2(2000) not null,
@@ -30,10 +29,10 @@ create table freeBoardTBL(bno number(10,0) constraint freeBoardTBL_pk primary ke
                       updatedate date default sysdate,
                       REPLYCNT number);
                       
--- �����Խ��� ������
+-- 자유게시판 시퀀스
 create sequence seq_freeBoard;
                       
--- ȫ���Խ��� ���̺�
+-- 홍보게시판 테이블
 create table proBoardTBL(bno number(10,0) constraint proBoardTBL_pk primary key,
                       title varchar2(200) not null,
                       content varchar2(2000) not null,
@@ -42,10 +41,10 @@ create table proBoardTBL(bno number(10,0) constraint proBoardTBL_pk primary key,
                       updatedate date default sysdate,
                       REPLYCNT number);
 
--- ȫ���Խ��� ������
+-- 홍보게시판 시퀀스
 create sequence seq_proBoard;
 
--- ���ǰԽ��� ���̺� 
+-- 문의게시판 테이블 
 create table inqBoardTBL(bno number(10,0) constraint inqBoardTBL_pk primary key,
                       title varchar2(200) not null,
                       content varchar2(2000) not null,
@@ -54,46 +53,46 @@ create table inqBoardTBL(bno number(10,0) constraint inqBoardTBL_pk primary key,
                       updatedate date default sysdate,
                       REPLYCNT number);
                       
--- ���ǰԽ��� ������
+-- 문의게시판 시퀀스
 create sequence seq_inqBoard;
 
--- �����Խ��� ��� ���̺�           
-create table freeReplyTBL(rno number(10,0) constraint pk_freereply primary key, --��� �۹�ȣ
-						  bno number(10,0) not null, --���� �۹�ȣ
-						  reply varchar2(1000) not null, --��� ����
-						  replyer varchar2(50) not null, --��� �ۼ���
+-- 자유게시판 댓글 테이블           
+create table freeReplyTBL(rno number(10,0) constraint pk_freereply primary key, --댓글 글번호
+						  bno number(10,0) not null, --원번 글번호
+						  reply varchar2(1000) not null, --댓글 내용
+						  replyer varchar2(50) not null, --댓글 작성자
 						  replydate date default sysdate,
 						  updatedate date default sysdate,
 						  constraint fk_freeReplyTBL foreign key(bno) references freeBoardTBL(bno));
 
--- �����Խ��� ��� ������	  
+-- 자유게시판 댓글 시퀀스	  
 create sequence seq_freeReply;
 
--- ȫ�� �Խ��� ��� ���̺�           
-create table proReplyTBL(rno number(10,0) constraint pk_proreply primary key, --��� �۹�ȣ
-						  bno number(10,0) not null, --���� �۹�ȣ
-						  reply varchar2(1000) not null, --��� ����
-						  replyer varchar2(50) not null, --��� �ۼ���
+-- 홍보 게시판 댓글 테이블           
+create table proReplyTBL(rno number(10,0) constraint pk_proreply primary key, --댓글 글번호
+						  bno number(10,0) not null, --원번 글번호
+						  reply varchar2(1000) not null, --댓글 내용
+						  replyer varchar2(50) not null, --댓글 작성자
 						  replydate date default sysdate,
 						  updatedate date default sysdate,
 						  constraint fk_proReplyTBL foreign key(bno) references proboardTBL(bno));
 
--- ȫ���Խ��� ��� ������	  
+-- 홍보게시판 댓글 시퀀스	  
 create sequence seq_proReply;
 
--- ���ǰԽ��� ��� ���̺�           
-create table inqReplyTBL(rno number(10,0) constraint pk_inqreply primary key, --��� �۹�ȣ
-						  bno number(10,0) not null, --���� �۹�ȣ
-						  reply varchar2(1000) not null, --��� ����
-						  replyer varchar2(50) not null, --��� �ۼ���
+-- 문의게시판 댓글 테이블           
+create table inqReplyTBL(rno number(10,0) constraint pk_inqreply primary key, --댓글 글번호
+						  bno number(10,0) not null, --원번 글번호
+						  reply varchar2(1000) not null, --댓글 내용
+						  replyer varchar2(50) not null, --댓글 작성자
 						  replydate date default sysdate,
 						  updatedate date default sysdate,
 						  constraint fk_inqReplyTBL foreign key(bno) references inqboardTBL(bno));
 
--- ���ǰԽ��� ��� ������	  
+-- 문의게시판 댓글 시퀀스	  
 create sequence seq_inqReply;
 
--- ���� �Խ��� ����÷�� ���̺�
+-- 자유 게시판 파일첨부 테이블
 create table free_file_attach(uuid varchar2(100) not null,
                          uploadPath varchar2(200) not null,
                          fileName varchar2(100) not null,
@@ -103,7 +102,7 @@ create table free_file_attach(uuid varchar2(100) not null,
 alter table file_attach add constraint pk_attach primary key(uuid);
 alter table file_attach add constraint fk_file_attach foreign key(bno) references freeboardTBL(bno);
 
--- ȫ�� �Խ��� ����÷�� ���̺�
+-- 홍보 게시판 파일첨부 테이블
 create table pro_file_attach(uuid varchar2(100) not null,
                          uploadPath varchar2(200) not null,
                          fileName varchar2(100) not null,
@@ -113,7 +112,7 @@ create table pro_file_attach(uuid varchar2(100) not null,
 alter table pro_file_attach add constraint pk_proattach primary key(uuid);
 alter table pro_file_attach add constraint fk_file_attach foreign key(bno) references proboardTBL(bno);
 
--- ���� �Խ��� ����÷�� ���̺�
+-- 문의 게시판 파일첨부 테이블
 create table inq_file_attach(uuid varchar2(100) not null,
                          uploadPath varchar2(200) not null,
                          fileName varchar2(100) not null,
@@ -123,7 +122,7 @@ create table inq_file_attach(uuid varchar2(100) not null,
 alter table inq_file_attach add constraint pk_inqattach primary key(uuid);
 alter table inq_file_attach add constraint fk_inqfile_attach foreign key(bno) references inqboardTBL(bno);
 
--- ���� �Խ��� ����÷�� ���̺�
+-- 리뷰 게시판 파일첨부 테이블
 create table review_file_attach(uuid varchar2(100) not null,
                          uploadPath varchar2(200) not null,
                          fileName varchar2(100) not null,
@@ -135,9 +134,7 @@ alter table review_file_attach add constraint fk_reviewfile_attach foreign key(b
 
 drop table reviewTBL;
                          
--- ���� ���̺�
+-- 권한 테이블
 create table login_auth(userid varchar2(50) not null,
                         auth varchar2(50) not null,
                         constraint fk_member_auth foreign key(userid) references memberTBL(userid));
-                        
-
